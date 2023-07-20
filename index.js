32d3857f7f7be08897bdd3ffad550c6c24f4e814
+ <+>
2023-07-20 11:19:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (location){
+    if(location>42){
+        return location-42
+    } else {
+        return 42-location
+    }
+
+}
+ function distancefromHQinfeet(location){
+    let distance = (location -42) * 264;
+    if (distance = < 0){
+        return (distance * -1)
+    }
+ }
+function distancetravelledinfeet(start,destination){
+    let travel = (destination - start) * 264
+    if (travel = (destination - start) * 264
+    if (travel < 0){
+        return (travel * -1)
+    }
+    else{
+        return travel;
+    }
+}
+
+const currentuser = 'grace hopper'
+
+
